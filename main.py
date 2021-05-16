0af7672b651b685a54f26389d220e427fb6ff3a7
yupenglei <yupengniaosanshi@gmail.com>
2021-05-16 23:55:16 +0800

    实现增删改查功能

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from typing import Optional, List
-
+import functools
+from typing import List, Callable
 from fastapi import FastAPI
 from pydantic import BaseModel
+
 from beans import Task
 
 app = FastAPI()
 
 
 class TaskJsonItem(BaseModel):
-    id: int
     name: str
     content: str
 
 
-def mock_tasks() -> list[Task]:
+def mock_tasks() -> List[Task]:
     _arr: List[Task] = []
     for i in range(5):
         task = Task(i, f"name{i}", f"content{i}")
         _arr.append(task)
     return _arr
+
+
+def find_item(item_list: List[Task], predictor: Callable[[Task], bool]) -> List[Task]:
+    return [_item for _item in item_list if predictor(_item)]
 
 
 arr = mock_tasks()
@@ -30,19 +34,42 @@ def read_root():
 
 
 @app.get("/tasks")
-def get_tasks():
+def select_task_list():
     return arr
 
 
+@app.get("/tasks/{task_id}")
+def select_task(task_id: int):
+    _arr = find_item(arr, lambda it: it.id == task_id)
+    if len(_arr) != 0:
+        return _arr[0]
+    return None
+
+
 @app.delete("/tasks/{task_id}")
-def delete_item(task_id: int):
-    _arr = [_item for _item in arr if _item.id == task_id]
+def delete_task(task_id: int):
+    _arr = find_item(arr, lambda it: it.id == task_id)
     print(f'delete item: {_arr}, len={len(_arr)}')
     for _item in _arr:
         arr.remove(_item)
-    return {"success": len(_arr) > 0}
+    return len(_arr) > 0
 
 
-@app.get("/tasks/{task_id}")
-def read_item(task_id: int, q: Optional[str] = None):
-    return {"item_id": task_id, "q": q}
+@app.put("/tasks/add")
+def insert_task(item: TaskJsonItem):
+    _next_id = functools.reduce(max, map(lambda it: it.id, arr)) + 1
+    _task = Task(_next_id, item.name, item.content)
+    arr.append(_task)
+    return _next_id
+
+
+@app.post('/tasks/{task_id}')
+def update_task(task_id: int, item: TaskJsonItem):
+    print(f'task_id={task_id}, item={item}')
+    _arr = find_item(arr, lambda it: it.id == task_id)
+    if len(_arr) == 0:
+        return -1
+    _task = _arr[0]
+    _task.name = item.name
+    _task.content = item.content
+    return _task.id
